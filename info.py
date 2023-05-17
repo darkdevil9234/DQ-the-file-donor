cc3f2bfc35f09fcc8ef6911319f084299e5a70a7
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2023-05-17 18:07:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,9 +48,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 IS_VERIFY = bool(environ.get('IS_VERIFY', False))
-VERIFY2_URL = environ.get('VERIFY2_URL', "Tnlink.in")
+VERIFY2_URL = environ.get('VERIFY2_URL', "Tnshort.net")
 VERIFY2_API = environ.get('VERIFY2_API', "811e36e9822b8c445582837748258bc00f7c6117")
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Tnlink.in')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Tnshort.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '811e36e9822b8c445582837748258bc00f7c6117')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
